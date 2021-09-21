2fd09893beb6c64ee76e8f498c9305d22016f09e
Natan <enderbright@gmail.com>
2021-09-21 13:16:04 +0200

    update

--- zad2/index.php
+++ zad2/index.php
@@ -5,6 +5,24 @@ if(isset($_POST['a']) && isset($_POST['b'])) {
     $b = $_POST['b'];
 
     echo $a * $b;
+    echo "<br>";
+    echo 2 * $a + 2 * $b;
 } else {
-    include_once 'formularz.html';
+    echo '
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="index.php" method="post">
+            <input type="number" name="a">
+            <input type="number" name="b">
+            <button type="submit">SUBMIT</button>
+        </form>
+    </body>
+    </html>';
 }
